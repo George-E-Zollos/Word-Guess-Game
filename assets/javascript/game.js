d58b1d459b85f8259591da39178f3bbb5f6eaf5a
gzoll001 <gzoll001@gmail.com>
2020-05-03 13:40:35 -0400

    Added to JS file creating guessing functionality. Next step will require me to add in the ability to determine if a guess was wrong and adjust lives accordingly.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,14 @@ var songList = [
   "Margaritaville",
 ];
 
-//document.getElementById('mysteryWord').innerHTML = randomSong;
+let randomSong = songList[Math.floor(Math.random() * songList.length)];
+let unknown = randomSong.split("");
+let underscores = [];
+let guess;
 
 /*-------------------- Function that sets up a random song name and assigns it to a variable. It then will display a number of underscores equal to the length of the song name ----------------------*/
+
 function mysteryWordGenerator() {
-
-  let randomSong = songList[Math.floor(Math.random() * songList.length)];
-  let unknown = randomSong.split("");
-  let underscores = [];
-
   unknown.forEach((x) => {
     if (x != " ") {
       underscores.push("_");
@@ -26,26 +25,26 @@ function mysteryWordGenerator() {
   });
 
   console.log(underscores);
-  document.getElementById("mysteryWord").innerHTML = underscores;
+  document.getElementById("mysteryWord").innerHTML = underscores.join("");
+}
+
+/*-------------------- Function that logs any key pressed to the "guess" variable, it then executes the letterCheck function -------------------*/
+
+document.onkeyup = function listener(event) {
+  guess = event.key;
+  console.log(guess);
+  letterCheck();
+};
+
+/*------------------- Compares the guessed letter to the unknown array and determines if there are matches, it then assigns value to underscores based on index ----------------*/
+
+function letterCheck() {
+  unknown.forEach((x, i) => {
+    if (guess.localeCompare(unknown[i], "en", { sensitivity: "base" }) == 0) {
+      underscores[i] = unknown[i];
+    }
+    document.getElementById("mysteryWord").innerHTML = underscores.join("");
+  });
 }
 
 mysteryWordGenerator();
-
-// const items = ['item1', 'item2', 'item3']
-// const copyItems = []
-
-// // before
-// for (let i = 0; i < items.length; i++) {
-//   copyItems.push(items[i])
-// }
-
-// // after
-// items.forEach(function(item){
-//   copyItems.push(item)
-// })
-
-// function foreach (arr) {
-//     for(arr.length) {
-//         // return value at index
-//     }
-// }
